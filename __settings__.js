146ba66e6b125cf40d72520889436a9d324e3d40
Elijah Newman-Gomez <smsithlord@yahoo.com>
2023-02-13 10:28:28 -0800

    GLB Demo initial

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1661328.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 121317548, 121317556, 121317558, 121317572, 121317575, 121317582 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/121317545/1/basis.wasm.js', 'wasmUrl' : 'files/assets/121317546/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/121317544/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/121317541/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/121317542/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/121317540/1/ammo.js', 'preload' : true},
+];
